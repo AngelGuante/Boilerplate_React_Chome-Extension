7fdd12191780d77760f8aeade0917152f5fb48d4
Miguel Angel Guante Suero <49294128+AngelGuante@users.noreply.github.com>
2023-07-23 09:44:03 -0400

    Adding basic chrome tabs api implementation

--- src/popup.jsx
+++ src/popup.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 function Popup() {
+    const OpenGoogleOnTab = () => {
+        // This allows us to use the Chrome Tabs API.
+        // https://developer.chrome.com/docs/extensions/reference/tabs/ For more information.
+        chrome.tabs.create({
+            url: "https://www.google.com/"
+        })
+    }
+
     return (
         <div>
             <h1>Hello Extensions</h1>
             <p>This is the React chrome extension boilerplate</p>
+            <span>Go to <a href="" onClick={OpenGoogleOnTab}>https://www.google.com/</a> and look if it's work!</span>
         </div>
     )
 }
